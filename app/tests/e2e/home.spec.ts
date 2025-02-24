8ca6754fed603bc1b3f86563711f5ae1c705c681
cedric-pouilleux <mino.mnz@gmail.com>
2025-02-24 14:01:53 +0100

    Try to use playwright image for staging e2e test on CI

--- app/tests/e2e/home.spec.ts
+++ app/tests/e2e/home.spec.ts
@@ -3,13 +3,6 @@ import { test, expect } from "@playwright/test";
 test("Home page should load and display content correctly", async ({
   page,
 }) => {
-  const response = await page.goto("http://localhost:3200", { timeout: 5000 });
-
-  if (!response || response.status() !== 200) {
-    throw new Error("🚨 Page non accessible ! Le serveur est peut-être OFF.");
-  }
-
-  console.log("DEBUG PAGE CONTENT:", await page.content());
-
+  await page.goto(process.env.BASE_URL || "http://localhost:3200");
   await expect(page).toHaveTitle(/Welcome to Fuck!/);
 });
