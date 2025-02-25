17e237fe68e8357601934c341ff15db79f9079ca
cedric-pouilleux <mino.mnz@gmail.com>
2025-02-25 02:29:43 +0100

    Use sha commit at docker hub image push

--- app/components/Test.vue
+++ app/components/Test.vue
@@ -3,7 +3,7 @@
 </template> 
 
 <script lang="ts" setup>
-    defineProps<{
+    defineProps<{ 
         title: string;
     }>()
 </script>
--- app/components/init.test.ts
+++ app/components/init.test.ts
@@ -1,5 +1,5 @@
 import { describe, it, expect } from "vitest";
-import { mount, shallowMount } from "@vue/test-utils";
+import { mount } from "@vue/test-utils";
 import Test from "./Test.vue";
 
 describe("Test init", () => {
