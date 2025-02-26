c94d20449fd087a40fa28b29be16237bc07ddc96
cedric-pouilleux <mino.mnz@gmail.com>
2025-02-26 02:45:35 +0100

    Try to execute playwright testing on github VM instead of docker

--- app/tests/e2e/home.spec.ts
+++ app/tests/e2e/home.spec.ts
@@ -1,6 +1,6 @@
 import { test, expect } from '@playwright/test'
 
 test('Home page should load and display content correctly', async ({ page }) => {
-  await page.goto(process.env.BASE_URL || 'http://localhost:3200')
+  await page.goto(process.env.BASE_URL || 'http://localhost:3000')
   await expect(page).toHaveTitle(/Welcome to Nuxt!/)
 })
