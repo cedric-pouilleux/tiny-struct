5e6b9134e76729ceecea9094467c184bc4f74344
cedric-pouilleux <mino.mnz@gmail.com>
2025-03-06 19:10:39 +0100

    Add NuxtImg, nuxt-auth-utils with google provider, make log and unlog bootstrap

--- app/nuxt.config.ts
+++ app/nuxt.config.ts
@@ -1,5 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+
 export default defineNuxtConfig({
+  modules: ['@pinia/nuxt', 'nuxt-auth-utils', '@nuxt/icon', '@nuxt/image'],
+  ssr: true,
   compatibilityDate: '2024-11-01',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  image: {
+    domains: ['lh3.googleusercontent.com']
+  }
 })
